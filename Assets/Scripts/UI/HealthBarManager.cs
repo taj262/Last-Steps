a8aa0fe16ed5ac4d64ef8603abdbab9798083062
taj262 <taj262@nau.edu>
2023-09-28 21:56:17 -0700

    Add HealthBarManager to its own UI folder

--- Assets/Scripts/UI/HealthBarManager.cs
+++ Assets/Scripts/UI/HealthBarManager.cs
@@ -16,6 +16,6 @@ public class HealthBarManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        healthBar.fillAmount = hp.currentHealthPercent();
+        healthBar.fillAmount = hp.CurrentHealthPercent();
     }
 }
